564657bfa4e28b143d31401b72862432328112fe
CodeRAHULx <rahul565157@gmail.com>
2026-03-25 23:59:24 +0530

    new easy level question

---
+++ arrays/Leetcode/Easy/palindrome.java
@@ -0,0 +1,31 @@
+package Leetcode.Easy;
+
+public class palindrome {
+
+
+
+        public static void main(String[] args) {
+            int x = 121;
+
+            boolean result = isPalindrome(x);
+
+            System.out.println(result);
+        }
+
+        public static boolean isPalindrome(int x) {
+
+            if(x < 0) return false;
+
+            int original = x;
+            int rev = 0;
+
+            while(x != 0) {
+                int digit = x % 10;
+                rev = rev * 10 + digit;
+                x = x / 10;
+            }
+
+            return original == rev;
+        }
+    }
+
